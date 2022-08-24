c9ec6c65a8312670f6edacc1eee06ec7622d6587
HanZawNyein <hanzawnyineonline@gmail.com>
2022-08-24 22:28:57 +0630

    start app

---
+++ android/app/src/main/kotlin/com/example/doc_tester/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.doc_tester
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    const Center(
+      child: Text(
+        "Hello, World",
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
